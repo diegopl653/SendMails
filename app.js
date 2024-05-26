5ce12656e277b56f31c7c6b0bd6b5f99b087e636
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-05-25 23:56:06 -0400

    refactor: project reestructure and pug added

--- app.js
+++ app.js
@@ -1,40 +1,4 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
-
-const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: process.env.USER,
-        pass: process.env.PASSWORD,
-    }
-});
-
-const template = {
-    helloworld: '<p>Prueba whit dotenv</p>'
-};
-
-async function sendMail(data) {
-    try {
-        let mailOptions = {
-            from: "pruebas.dieguito@gmail.com",
-            to: data.mail,
-            subject: data.subject,
-            text: data.text,
-            html: template.helloworld
-        };
-
-        let info = await transporter.sendMail(mailOptions);
-        console.log("mensaje enviado: ", info.messageId);
-        return info.messageId;
-    } catch (error) {
-        console.log("Error al enviar el correo: ", error)
-        throw error;
-    }
-};
-
-module.exports = {
-    sendMail
-};
+const { sendMail } = require("./interface-adapters/emailService/nodemailerEmailService")
 
 const emailData = {
     mail: "dgprzln@gmail.com",
@@ -42,3 +6,5 @@ const emailData = {
     text: "Hola mundo",
     template: "HelloWorld",
 };
+
+sendMail(emailData)
---
+++ interface-adapters/emailService/nodemailerEmailService.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: process.env.USER,
+        pass: process.env.PASSWORD,
+    }
+});
+
+const template = {
+    helloworld: '<p>Prueba whit dotenv</p>'
+};
+
+async function sendMail(data) {
+    try {
+        let mailOptions = {
+            from: "pruebas.dieguito@gmail.com",
+            to: data.mail,
+            subject: data.subject,
+            text: data.text,
+            html: template.helloworld
+        };
+
+        let info = await transporter.sendMail(mailOptions);
+        console.log("mensaje enviado: ", info.messageId);
+        return info.messageId;
+    } catch (error) {
+        console.log("Error al enviar el correo: ", error)
+        throw error;
+    }
+};
+
+module.exports = {
+    sendMail
+};
